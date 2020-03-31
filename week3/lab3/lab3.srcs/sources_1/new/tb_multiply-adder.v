2c02989812c38709acf6d03cc7ca98d29b5b8731
givenone <givenone1@gmail.com>
2020-03-31 16:29:30 +0900

    Done lab 4

---
+++ week3/lab3/lab3.srcs/sources_1/new/tb_multiply-adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/03/31 14:22:27
+// Design Name: 
+// Module Name: tb_multiply-adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_multiply-adder(
+
+    );
+endmodule
