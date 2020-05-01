591250f6d2c3a9d7f08fb9b682a7b4c433483900
givenone <givenone1@gmail.com>
2020-05-01 18:51:24 +0900

    testing pe controller

--- lab6/lab6.srcs/sources_1/new/pe_controller.v
+++ lab6/lab6.srcs/sources_1/new/pe_controller.v
@@ -11,7 +11,16 @@ module pe_controller #(
         input [31:0] rddata,
         output done,
         output [L_RAM_SIZE : 0] raddr, // LRAM * 2, be careful of size
-        output reg [31:0] wrdata
+        output reg [31:0] wrdata,
+        output [2:0] state,
+        output [2:0] n_state,
+        output [31:0] c,
+        output [31:0] ddd,
+        output dval,
+        output [31:0] a,
+        output [31:0] d,
+        output w,
+        output [L_RAM_SIZE-1:0]  hh_addr
     );
     
     // register and wires for pe module !
@@ -28,7 +37,7 @@ module pe_controller #(
     (* ram_style = "block" *) reg [31:0] peram [0: VECTOR_SIZE - 1];  // global register
     always @(posedge aclk)
         if(we_global) peram[addr] <= rddata;
-        else gout <= peram[addr];    
+        //else gout <= peram[addr];    
     
     // FSM
     parameter 	IDLE = 3'd0, INIT = 3'd1, LOAD = 3'd2, CALC = 3'd3, DONE = 3'd4;        
@@ -36,6 +45,9 @@ module pe_controller #(
     wire calc_done;
     wire done_done;
     reg [2:0] present_state, next_state;
+    
+    assign state = present_state;
+    assign n_state = next_state;
     
     // state shift
     always @(posedge aclk)
@@ -53,16 +65,17 @@ module pe_controller #(
     // COUNTER -> implement only one counter ! -> zero is done.
             
     localparam count_load = 2 * 2 * VECTOR_SIZE - 1; // 
-    localparam count_cal = VECTOR_SIZE - 1;  // 16
+    localparam count_cal = VECTOR_SIZE * VECTOR_SIZE - 1;  // 16
     localparam count_done = 5; // 5
     reg [31:0] counter;
-    wire [31:0] counter_val = (present_state == LOAD) ? count_load 
-    : (present_state == CALC) ?  count_cal 
-    : (present_state == DONE) ? count_done 
+    assign c = counter;
+    wire [31:0] counter_val = (next_state == LOAD) ? count_load 
+    : (next_state == CALC) ?  count_cal 
+    : (next_state == DONE) ? count_done 
     : 0;
     
     wire counter_start =  start || load_done || calc_done;
-    wire counter_down = (present_state == LOAD) || dvalid || (present_state == DONE);
+    wire counter_down = (present_state == LOAD) || (present_state == CALC) || (present_state == DONE);
     always @(posedge aclk) begin
         if(!areset_n) counter <= 'd0;
         else 
@@ -74,7 +87,7 @@ module pe_controller #(
     assign load_done = (present_state == LOAD) && (counter == 'd0);
     assign calc_done = (present_state == CALC) && (counter == 'd0) && dvalid;
     assign done_done = (present_state == DONE) && (counter == 'd0);
-    assign done = done_done;
+    assign done = (present_state == DONE);
     
     // internal registers
     // 1) LOAD - global/local register -> we / addr setting, raddr, din
@@ -84,7 +97,7 @@ module pe_controller #(
     // if counter[0] == 0 -> read && counter[0] == 1 -> write
     
     assign addr = (present_state == LOAD) ? counter[L_RAM_SIZE:1] : // applicable for both global, local RAM
-    (present_state == CALC) ? counter[L_RAM_SIZE-1:0] : 1'd0;
+    (present_state == CALC) ? counter[31:4] : 1'd0;
     
     // raddr : address for 2 RAMs
     assign raddr = (present_state == LOAD) ? counter[L_RAM_SIZE+1:1] : 1'd0;
@@ -92,7 +105,7 @@ module pe_controller #(
     assign din = (present_state == LOAD) ? rddata : 1'd0;
     
     // 2) CALC
-    assign ain = (present_state == CALC) ? gout : 1'd0;
+    assign ain = (present_state == CALC) ? peram[addr] : 1'd0;
     assign valid = (present_state == CALC) ? 1'd1 : 1'd0;
     
     // 3) DONE
@@ -100,7 +113,7 @@ module pe_controller #(
         if (!areset_n)
                 wrdata <= 'd0;
         else
-            if (calc_done) wrdata <= dout;
+            if (dvalid) wrdata <= dout;
             else wrdata <= wrdata;
             
     my_pe #(.L_RAM_SIZE(6)) mac(
@@ -114,4 +127,11 @@ module pe_controller #(
     .dvalid(dvalid),
     .dout(dout)
     );
+    
+    assign ddd = dout;
+    assign dval = dvalid;
+    assign a = ain;
+    assign d = din;
+    assign w = we_local;
+    assign hh_addr = addr;
 endmodule
